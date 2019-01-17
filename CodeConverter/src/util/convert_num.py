f303f2c52e6247897a310cc14b512c6938b9de1f
Kevin-san <34958130+Kevin-san@users.noreply.github.com>
2019-01-17 17:12:46 +0800

    Update convert_num.py

--- CodeConverter/src/util/convert_num.py
+++ CodeConverter/src/util/convert_num.py
@@ -2,6 +2,99 @@
 # -*- coding: utf-8 -*-
 
 import os,sys
+import re
+
+import qrcode
+from PIL import Image
+from pyzbar import pyzbar
+
+
+def make_qr_code_easy(content, save_path=None):
+    """
+    Generate QR Code by default
+    :param content: The content encoded in QR Codeparams
+    :param save_path: The path where the generated QR Code image will be saved in.
+                      If the path is not given the image will be opened by default.
+    """
+    img = qrcode.make(data=content)
+    if save_path:
+        img.save(save_path)
+    else:
+        img.show()
+
+
+def make_qr_code(content, save_path=None):
+    """
+    Generate QR Code by given params
+    :param content: The content encoded in QR Code
+    :param save_path: The path where the generated QR Code image will be saved in.
+                      If the path is not given the image will be opened by default.
+    """
+    qr_code_maker = qrcode.QRCode(version=2,
+                                  error_correction=qrcode.constants.ERROR_CORRECT_M,
+                                  box_size=8,
+                                  border=1,
+                                  )
+    qr_code_maker.add_data(data=content)
+    qr_code_maker.make(fit=True)
+    img = qr_code_maker.make_image(fill_color="black", back_color="white")
+    if save_path:
+        img.save(save_path)
+    else:
+        img.show()
+
+
+def make_qr_code_with_icon(content, icon_path, save_path=None):
+    """
+    Generate QR Code with an icon in the center
+    :param content: The content encoded in QR Code
+    :param icon_path: The path of icon image
+    :param save_path: The path where the generated QR Code image will be saved in.
+                      If the path is not given the image will be opened by default.
+    :exception FileExistsError: If the given icon_path is not exist.
+                                This error will be raised.
+    :return:
+    """
+    if not os.path.exists(icon_path):
+        raise FileExistsError(icon_path)
+
+    # First, generate an usual QR Code image
+    qr_code_maker = qrcode.QRCode(version=4,
+                                  error_correction=qrcode.constants.ERROR_CORRECT_H,
+                                  box_size=8,
+                                  border=1,
+                                  )
+    qr_code_maker.add_data(data=content)
+    qr_code_maker.make(fit=True)
+    qr_code_img = qr_code_maker.make_image(fill_color="black", back_color="white").convert('RGBA')
+
+    # Second, load icon image and resize it
+    icon_img = Image.open(icon_path)
+    code_width, code_height = qr_code_img.size
+    icon_img = icon_img.resize((code_width // 4, code_height // 4), Image.ANTIALIAS)
+
+    # Last, add the icon to original QR Code
+    qr_code_img.paste(icon_img, (code_width * 3 // 8, code_width * 3 // 8))
+
+    if save_path:
+        qr_code_img.save(save_path)
+    else:
+        qr_code_img.show()
+
+
+def decode_qr_code(code_img_path):
+    """
+    Decode the given QR Code image, and return the content
+    :param code_img_path: The path of QR Code image.
+    :exception FileExistsError: If the given code_img_path is not exist.
+                                This error will be raised.
+    :return: The list of decoded objects
+    """
+    if not os.path.exists(code_img_path):
+        raise FileExistsError(code_img_path)
+
+    # Here, set only recognize QR Code and ignore other type of code
+    return pyzbar.decode(Image.open(code_img_path), symbols=[pyzbar.ZBarSymbol.QRCODE])
 
 def bin2oct(string_num):
 	return oct(int(string_num,2))
@@ -68,6 +161,39 @@ def strB2Q(ustring):
         ss.append(rstring)
     return ''.join(ss)
 
+def convert(one_string,space_character):    #one_string:输入的字符串；space_character:字符串的间隔符，以其做为分隔标志
+    string_list = str(one_string).split(space_character)    #将字符串转化为list
+    first = string_list[0].lower()
+    others = string_list[1:]
+    others_capital = [word.capitalize() for word in others]      #str.capitalize():将字符串的首字母转化为大写
+    others_capital[0:0] = [first]
+    hump_string = ''.join(others_capital)     #将list组合成为字符串，中间无连接符。
+    return hump_string
+
+def hump2underline(hunp_str):
+    '''
+    驼峰形式字符串转成下划线形式
+    :param hunp_str: 驼峰形式字符串
+    :return: 字母全小写的下划线形式字符串
+    '''
+    # 匹配正则，匹配小写字母和大写字母的分界位置
+    p = re.compile(r'([a-z]|\d)([A-Z])')
+    # 这里第二个参数使用了正则分组的后向引用
+    sub = re.sub(p, r'\1_\2', hunp_str).lower()
+    return sub
+
+def underline2hump(underline_str):
+    '''
+    下划线形式字符串转成驼峰形式
+    :param underline_str: 下划线形式字符串
+    :return: 驼峰形式字符串
+    '''
+    if underline_str.isupper():
+        underline_str=underline_str.lower()
+    # 这里re.sub()函数第二个替换参数用到了一个匿名回调函数，回调函数的参数x为一个匹配对象，返回值为一个处理后的字符串
+    sub = re.sub(r'(_\w)',lambda x:x.group(1)[1].upper(),underline_str)
+    return sub
+
 if __name__=='__main__':
 	test_bin_str='0b1100100'
 	test_oct_str='0o144'
@@ -99,3 +225,30 @@ if __name__=='__main__':
 	file=open('/home/cx24793/convert_num.txt','w+',encoding='utf-8')
 	file.write(test_whole_str)
 	file.close()
+	
+	test_name='ANT_XML'
+	humpStr=underline2hump(test_name)
+	print(humpStr)
+	underlineStr=hump2underline(humpStr)
+	print(underlineStr)
+
+	make_qr_code_easy("make_qr_code_easy", "qrcode.png")
+	results = decode_qr_code("qrcode.png")
+	if len(results):
+		print(results[0].data.decode("utf-8"))
+	else:
+		print("Can not recognize.")
+	
+	make_qr_code("make_qr_code", "qrcode.png")
+	results = decode_qr_code("qrcode.png")
+	if len(results):
+		print(results[0].data.decode("utf-8"))
+	else:
+		print("Can not recognize.")
+	
+	make_qr_code_with_icon("https://blog.csdn.net/Zhipeng_Hou", "icon.jpg", "qrcode.png")
+	results = decode_qr_code("qrcode.png")
+	if len(results):
+		print(results[0].data.decode("utf-8"))
+	else:
+		print("Can not recognize.")
