6e7200a04ff5ea6620f36f5211e694204cbeb44c
Kevin-san <34958130+Kevin-san@users.noreply.github.com>
2019-01-21 16:55:25 +0800

    Update tkinter.py

--- CodeConverter/src/util/tkinter.py
+++ CodeConverter/src/util/tkinter.py
@@ -1,3 +1,214 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/5/9 下午1:36
+# @Author  : fgyong 简书:_兜兜转转_  https://www.jianshu.com/u/6d1254c1d145
+# @Site    : http://fgyong.cn 兜兜转转的技术博客
+# @File    : 12.py
+# @Software: PyCharm
+import sys,time,datetime
+from PyQt5.QtWidgets import (QMainWindow,QMessageBox,QWidget, QToolTip, QPushButton, QApplication, QGestureEvent,QLabel,QDesktopWidget)
+from PyQt5.QtGui import QFont,QIcon
+
+
+class Example(QWidget):
+  #构造函数
+    def __init__(self):
+        super().__init__()
+        self.initGUI();
+#初始化函数
+    def initGUI(self):
+#设置窗体frame
+         self.setGeometry(500, 300, 300, 300)
+#窗体title
+        self.setWindowTitle('我的第一个程序')
+#添加label
+        self.lable = QLabel('helloword!',self)
+#自动换行
+        self.lable.setWordWrap(True)
+#lable的frame
+        self.lable.setGeometry(50,100,200,150)
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    ex.show()
+    sys.exit(app.exec_())
+
+class Example(QWidget):
+    lable = ''
+    def __init__(self):
+        super().__init__()
+        self.initGUI();
+
+    def initGUI(self):
+        # QToolTip.setFont('SanSerif',10)
+#提示文字
+        self.setToolTip('<b>这是第一个QWidget</b>')
+#更新时间的button
+        btn = QPushButton('更新时间', self)
+#button的提示文字
+        btn.setToolTip('这是个btn')
+#设置默认大小
+        btn.resize(btn.sizeHint())
+
+        btn2 = QPushButton('alert', self)
+        btn2.setToolTip('这是弹出alert的btn')
+        btn2.resize(btn.sizeHint())
+        btn2.move(0,60)
+        btn2.clicked.connect(self.showMessage)
+
+        size = self.geometry()
+   #绑定button的点击函数   self.showTime是函数名字后边没有（）
+        btn.clicked.connect(self.showTime)
+
+        self.setGeometry(1500, 300, 300, 300)
+        self.setWindowTitle('我的第一个程序')
+
+        self.lable = QLabel('时间：',self)
+        self.lable.setWordWrap(True)#自动换行
+        self.lable.setGeometry(50,100,200,150)
+        self.lable.setText('时间:'+datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+        icon = QIcon("/Users/Jerry/PycharmProjects/Flask_dmeo_1/App/icons/cry.png")
+        btn.setIcon(icon)
+        self.setWindowIcon(icon)
+    def showTime(self):
+        nowTime=datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        self.lable.setText("时间:"+nowTime)
+
+    def showMessage(self):
+        self.showBox("alert message")
+    def showBox(self,message):
+        box=QMessageBox.question(self,'温馨提示',message,QMessageBox.Yes|QMessageBox.No,QMessageBox.No)
+        if box == QMessageBox.Yes:
+            self.lable.setText(self.lable.text()+'YES')
+        else:
+            self.lable.setText(self.lable.text() + 'NO')
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    ex.show()
+    sys.exit(app.exec_())
+
+def showMenu(self):
+        # 生成母菜单栏
+        menubar = self.menuBar()
+        #添加菜单
+        p = menubar.addMenu('p')
+        #菜单 p2
+        impMenu = QMenu('p2', self)
+        # 子菜单 p2 click
+        impAct = QAction('p2 click', self)
+        #绑定 action
+        impAct.triggered.connect(self.p2)
+        # 添加菜单 p2 click
+        impMenu.addAction(impAct)
+        # 添加菜单 p2
+        p.addMenu(impMenu)
+
+
+        newAct = QAction('p1', self)
+        newAct.triggered.connect(self.p1)
+        p.addAction(newAct)
+
+
+    def p1(self):
+        print("p1")
+
+    def p2(self):
+        print("p2")
+
+def showTextView(self):
+        self.b=QTextEdit(self)
+        self.b.setPlaceholderText('我是默认文字')
+        self.b.setGeometry(20,150,200,100)
+
+        btn3 = QPushButton('提交', self)
+        btn3.resize(btn3.sizeHint())
+        btn3.clicked.connect(self.print)
+        btn3.move(110,260)
+    def print(self):
+        #输出来文本框的汉字
+        print(self.b.toPlainText())
+
+********************************************************************
+pyinstaller -F path 
+编译成可执行文件
+path是想编译文件的路径
+
+会生成 dist文件夹和build文件夹,在dist文件夹下边生成一个可执行文件，
+文件的名字和编译文件的名字一样。双击就可以执行这个程序了。
+********************************************************************
+
+====================================================================
+
+class handler_demo(QWidget, Ui_demo):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+ 
+        # self inherit QWidget
+        self.table_widget.setContextMenuPolicy(Qt.CustomContextMenu)
+        self.table_widget.customContextMenuRequested.connect(self.custom_right_menu)
+ 
+    def custom_right_menu(self, pos):
+        menu = QMenu()
+        opt1 = menu.addAction("menu1")
+        opt2 = menu.addAction("menu2")
+        action = menu.exec_(self.table_widget.mapToGlobal(pos))
+        if action == opt1:
+            # do something
+            return
+        elif action == opt2:
+            # do something
+            return
+        else:
+            return
+
+==============================================================================
+class handler_demo(QWidget, Ui_demo):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+ 
+        # self inherit QWidget
+        self.table_widget.setContextMenuPolicy(Qt.ActionsContextMenu)
+        self.table_widget.customContextMenuRequested.connect(self.custom_right_menu)
+ 
+    def custom_right_menu(self, pos):
+        menu = QMenu()
+        opt1 = menu.addAction("menu1")
+        opt2 = menu.addAction("menu2")
+        action = menu.exec_(self.table_widget.mapToGlobal(pos))
+        if action == opt1:
+            # do something
+            return
+        elif action == opt2:
+            # do something
+            return
+        else:
+            return
+
+=============================================================================
+class handler_demo(QWidget, Ui_demo):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+ 
+    def contextMenuEvent(self, event):
+        menu = QMenu()
+        opt1 = menu.addAction("menu1")
+        opt2 = menu.addAction("menu2")
+        action = menu.exec_(self.table_widget.mapToGlobal(event.pos()))
+        if action == opt1:
+            # do something
+            return
+        elif action == opt2:
+            # do something
+            return
+ 
+        menu.move(event.pos())
+
+==============================================================================
 #!/usr/bin/env python3
 
 import platform
